aeadc86857c29cbb2b72072d62526abb933accb2
TH-Raju <rajukhan894200@gmail.com>
2022-11-01 21:54:34 +0600

    CRUD - Delete set

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -18,15 +18,14 @@ app.use(express.json())
 
 // MongoDB Connection
 
-
 const uri = "mongodb+srv://virtual-diary:azGf75Mwdar4gF7F@cluster0.ddpko0x.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
 
-
     try {
         const userCollection = client.db('virtualDiary').collection('users');
+
 
         // CRUD - Read setup
         app.get('/users', async (req, res) => {
@@ -42,8 +41,19 @@ async function run() {
             console.log(user);
             const result = await userCollection.insertOne(user);
             res.send(result);
-        })
+        });
+
+
+        // CRUD - Delete setup
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            // console.log('delete', id);
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        });
     }
+
     finally {
 
     }
@@ -59,5 +69,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+    console.log(`Server running on port ${port}`);
 })
