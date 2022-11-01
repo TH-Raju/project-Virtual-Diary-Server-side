74988419ed80f8717e4b44c66d1e758fe99a1cbe
TH-Raju <rajukhan894200@gmail.com>
2022-11-01 19:21:39 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -8,6 +9,37 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
+
+
+//uname and pass
+// user: virtual-diary
+// pass:  azGf75Mwdar4gF7F
+
+
+
+
+
+const uri = "mongodb+srv://virtual-diary:azGf75Mwdar4gF7F@cluster0.ddpko0x.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const userCollection = client.db('virtualDiary').collection('users');
+        const user = {
+            name: 'testing mongo',
+            email: 'aaa@mail.com'
+        }
+        const result = await userCollection.insertOne(user);
+        console.log(result);
+    }
+    finally {
+
+    }
+
+}
+run().catch(err => console.log(err));
+
+
 
 
 app.get('/', (req, res) => {
